bf625758d3314059f263f7ad81feacb8d87f9fc3
John Pham <john.pham@plexure.com>
2021-09-19 17:43:48 +1200

    Catalog Api

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,83 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct(string id) {
+            var product = await _repository.GetProduct(id);
+            if(product == null)
+            {
+                _logger.LogError($"Product  with id: {id}, not found.");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _repository.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        // This http post method create a project and then make another api call to get the Product by the product ID. and returns the product object in the response, hence <ActionRESULT<Product>>
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product); // GetProduct declare the http get product method, 
+        }
+
+        // This http put method update a project.
+        // There is no object awaited from the result and it's only a boolean response, hence <IActionResult> if don't want to return any specific type of response.
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+
+        [HttpDelete("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
+        }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            // Create mongo client using the configurations supplied in the appsettings.json file. 
+            // To do this we need Iconfiguration Object to be injected.
+            // This is a feature of ASP.Net Core application.
+            // At compile time, if asp.net core see a IConfiguration object in the constructor it will create an instance of that case for us.
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:ProductDb"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
